768f275c24bf8a9bf2a575d3947977ae429e2243
limingguang <limingguang@localhost>
2017-03-10 00:02:49 +0800

    修改用户接口

--- src/main/java/com/sunshine/ebook/controller/UserController.java
+++ src/main/java/com/sunshine/ebook/controller/UserController.java
@@ -1,13 +1,14 @@
 package com.sunshine.ebook.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.sunshine.ebook.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 @Api(tags = "用户相关接口")
 @RestController
@@ -18,8 +19,8 @@ public class UserController {
 	private UserService userService;
 	
 	@ApiOperation(value = "根据邮箱判断是否注册")
-	@RequestMapping(value = "/checkEmailIsRegist/{email}", method = RequestMethod.GET)
-	public void checkEmailIsRegist(@PathVariable String email) {
+	@RequestMapping(value = "/checkEmailIsRegist", method = RequestMethod.GET)
+	public void checkEmailIsRegist(@ApiParam(value = "邮箱", required = true) @RequestParam("email") String email) {
 		int count = userService.checkEmailIsRegist(email);
 		String msg = "";
 		if (count > 0) {
